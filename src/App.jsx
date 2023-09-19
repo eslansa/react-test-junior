bacdecb59e8a9bf5783de0b57c33948624d72118
eslansa <eslansanchez12345@gmail.com>
2023-09-19 10:14:02 -0400

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
@@ -8,29 +9,33 @@ export function App () {
   const [fact, setFact] = useState('')
   const [imageUrl, setImageUrl] = useState()
 
+  //para recuperar la cita de carga rapida 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
-       const {fact} = data  
-       setFact(fact)
-
-       const firstWord = fact.split(' ', 3).join(' ')
-
-       fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`) 
-       .then(res => res.json())
-       .then(response => {
-        const {url} = response
-        setImageUrl(url)
-       })
-    })
+        const { fact } = data
+        setFact(fact)
+      })
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ', 3).join(' ')
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
 
   return (
     <main>
       <h1>App Gatitos</h1>
-      {fact && <p> {fact} </p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt='cat' />}
+        {fact && <p> {fact} </p>}
+        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt='cat' />}
     </main>
   )
 }
